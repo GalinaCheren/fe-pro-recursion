b4c3cfdfb52036c1a8842a1205be71516a910231
GalinaCheren <galinacheren8@gmail.com>
2023-02-16 23:56:16 +0200

    Home Work 35 fe-pro-recursion

--- src/index.js
+++ src/index.js
@@ -3,7 +3,22 @@
  * true, если есть различие, false. То есть проверяет каждое свойство, вне зависимости от вложенности,
  * делаем через рекурсию(а других вариантов и нет)
  */
-export const deepEqual = (obj, anotherObject) => {};
+export const deepEqual = (obj, anotherObject) => {
+  if (Object.keys(obj).length !== Object.keys(anotherObject).length) {
+    return false;
+  }
+
+  return Object.keys(obj).every((key) => {
+    if (
+      typeof obj[key] === 'object' &&
+      typeof anotherObject[key] === 'object'
+    ) {
+      return deepEqual(obj[key], anotherObject[key]);
+    } else {
+      return obj[key] === anotherObject[key];
+    }
+  });
+};
 
 /**
  * Принимает объект, возвращает его глубокую копию, то есть ни одно свойство
@@ -12,10 +27,43 @@ export const deepEqual = (obj, anotherObject) => {};
  * то тогда в рекурсию. С объектом также. Поскольку массив при typeof возвращает object, чтобы
  * их различить берем метод Array.isArray и он на массивах вернет тру
  */
-export const deepCopy = (obj) => {};
+export const deepCopy = (obj) => {
+  if (Array.isArray(obj)) {
+    return obj.map((element) => {
+      if (typeof element === 'object') {
+        return deepCopy(element);
+      }
+      return element;
+    });
+  } else {
+    const copyObj = {};
+    Object.keys(obj).forEach((key) => {
+      if (typeof obj[key] === 'object') {
+        copyObj[key] = deepCopy(obj[key]);
+      } else {
+        copyObj[key] = obj[key];
+      }
+    });
+    return copyObj;
+  }
+};
 
 /**
  * Мы передаем объект, и должны вернуть массив уникальных названий свойств
  * То есть если у нас объект { name: { bohdan: { name: 'test' } } } вернет ['name', 'bohdan']
  */
-export const getAllObjectKeys = (obj) => {};
+export const getAllObjectKeys = (obj) => {
+  const keys = new Set();
+
+  Object.entries(obj).forEach(([key, value]) => {
+    keys.add(key);
+    if (typeof value === 'object') {
+      getAllObjectKeys(value).forEach((k) => keys.add(k));
+    }
+  });
+  const result = [];
+  keys.forEach((key) => {
+    result.push(key);
+  });
+  return result;
+};
